c596506861f019ae8729f1ca489bd203825d74df
Kamrul Jaman <kamruljaman@Kamruls-MacBook-Pro.local>
2023-03-28 20:18:09 +0600

    create items view with items and end of line

---
+++ app/src/main/java/com/shopping/app/adapder/ItemsAdapter.java
@@ -0,0 +1,112 @@
+package com.shopping.app.adapder;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.shopping.app.R;
+import com.shopping.app.model.Item;
+
+import java.util.List;
+
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ItemsViewHolder> {
+
+    private static final int VIEW_TYPE_1 = 1;
+    private static final int VIEW_TYPE_2 = 2;
+    private final List<Item> items;
+
+    public ItemsAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ItemsViewHolder onCreateViewHolder
+            (ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_1) {
+            // Inflate layout for view type 1
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.item_view_layout, parent, false);
+            return new ItemsViewHolder(view);
+        } else {
+            // Inflate layout for view type 2
+            View view = LayoutInflater.from(parent.getContext())
+                    .inflate(R.layout.end_item_view_layout, parent, false);
+            return new EndOfResultsViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ItemsViewHolder holder, int position) {
+        if (position < items.size()) {
+            // Get the item at the current position in the list
+            Item item = items.get(position);
+
+            // Bind the data to the ViewHolder
+            holder.bindData(item);
+        } else {
+            holder.bindData(null);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size() + 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position != items.size()) {
+            return VIEW_TYPE_1;
+        } else {
+            return VIEW_TYPE_2;
+        }
+    }
+
+    public static class ItemsViewHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView itemImage;
+        private final TextView itemTitle;
+        private final TextView itemPrice;
+
+        public ItemsViewHolder(@NonNull View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View.
+            view.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(v.getContext(), "Item clicked: " + itemTitle.getText(), Toast.LENGTH_SHORT).show();
+                }
+            });
+            itemImage = view.findViewById(R.id.item_image_id);
+            itemTitle = view.findViewById(R.id.item_title_id);
+            itemPrice = view.findViewById(R.id.item_price_id);
+        }
+
+        public void bindData(Item item) {
+            // Set data for each item view in the RecyclerView
+            itemImage.setImageResource(R.drawable.macbook_pro);
+            itemTitle.setText(item.getTitle());
+            itemPrice.setText("Price: $" + item.getPrice());
+        }
+    }
+
+    // view holder for 'end of line'
+    public static class EndOfResultsViewHolder extends ItemsViewHolder {
+        public EndOfResultsViewHolder(@NonNull View view) {
+            super(view);
+            // Define click listener for the ViewHolder's View.
+            view.setOnClickListener(v -> Toast.makeText(v.getContext(), "End of result!", Toast.LENGTH_SHORT).show());
+        }
+
+        public void bindData(Item item) {
+        }
+    }
+}
--- app/src/main/java/com/shopping/app/fragments/ItemsFragment.java
+++ app/src/main/java/com/shopping/app/fragments/ItemsFragment.java
@@ -4,15 +4,39 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.shopping.app.R;
+import com.shopping.app.adapder.ItemsAdapter;
+import com.shopping.app.dao.ItemsDAO;
+import com.shopping.app.model.Item;
+
+import java.util.List;
 
 public class ItemsFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.items_layout, container, false);
+        View view = inflater.inflate(R.layout.items_layout, container, false);
+
+        recyclerView = view.findViewById(R.id.recyclerViewId);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 2));
+
+        ItemsDAO itemsDAO = new ItemsDAO();
+        itemsDAO.loadDummyData();
+
+        recyclerView.setAdapter(new ItemsAdapter(itemsDAO.getAll()));
+
+        return view;
     }
 }
